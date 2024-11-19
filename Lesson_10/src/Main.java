38da5d559c3a782988392e9a86cb8e321de20ece
Igor <pm2277@mail.ru>
2024-11-19 12:27:25 +0300

    Исправил ошибки, объединил, main вынес для двух классов

--- Lesson_10/src/Main.java
+++ Lesson_10/src/Main.java
@@ -10,6 +10,18 @@ public class Main {
         // Создаем объект треугольника с длинами сторон 3, 4 и 5, желтой заливкой и фиолетовой границей
         Shape triangle = new Triangle(3, 4, 5, "Желтый", "Фиолетовый");
 
+        Dog dog1 = new Dog(); // Создаем экземпляр собаки
+        dog1.run(300); // Собака пробегает 300 м
+        dog1.swim(5); // Собака плывет 5 м
+
+        Cat cat1 = new Cat(); // Создаем экземпляр кота
+        cat1.run(150); // Кот пробегает 150 м
+        cat1.swim(10); // Кот пытается плавать
+
+        FoodBowl bowl = new FoodBowl(5); // Создаем миску с 5 единицами еды
+        Cat[] cats = { new Cat(), new Cat(), new Cat() }; // Создаем массив из трех котов
+
+
         // Вывод характеристик круга
         System.out.println("Круг:");
         System.out.println("Периметр: " + circle.calculatePerimeter()); // Вывод периметра круга
@@ -32,5 +44,18 @@ public class Main {
         System.out.println("Площадь: " + triangle.calculateArea()); // Вывод площади треугольника
         System.out.println("Цвет заливки: " + triangle.getFillColor()); // Вывод цвета заливки треугольника
         System.out.println("Цвет границы: " + triangle.getBorderColor()); // Вывод цвета границы треугольника
+        // Вывод количества созданных животных, собак и котов
+        System.out.println("Количество созданных животных: " + Animal.getAnimalCount());
+        System.out.println("Количество созданных собак: " + Dog.getDogCount());
+        System.out.println("Количество созданных котов: " + Cat.getCatCount());
+        // Цикл для кормления котов из миски
+        for (Cat cat : cats) {
+            if (cat.eat(bowl)) { // Если кот поел
+                System.out.println("Кот поел и теперь сыт.");
+            } else {
+                System.out.println("Коту не хватило еды."); // Если еды не хватило
+            }
     }
+    }
+
 }
